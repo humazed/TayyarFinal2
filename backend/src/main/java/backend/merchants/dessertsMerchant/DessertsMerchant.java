57b645f9a2c2059906510aa2c015601f06dcc834
MuhammadSaeedBatikh <mohammud.saeed.batekh@gmail.co>
2017-09-23 02:20:23 +0200

    desserts merchant

---
+++ backend/src/main/java/backend/merchants/dessertsMerchant/DessertsMerchant.java
@@ -0,0 +1,22 @@
+package backend.merchants.dessertsMerchant;
+
+import com.googlecode.objectify.annotation.Cache;
+import com.googlecode.objectify.annotation.Subclass;
+
+import backend.merchants.Merchant;
+
+/**
+ * Created by Muhammad on 19/08/2017.
+ */
+@Subclass(index = true)
+@Cache
+public class DessertsMerchant extends Merchant {
+
+    //default constructor for Entity initialization
+    public DessertsMerchant(){}
+    //============
+
+    public DessertsMerchant(String name, String email, String phone, String imageURL) {
+        super(name, email, phone, imageURL);
+    }
+}
